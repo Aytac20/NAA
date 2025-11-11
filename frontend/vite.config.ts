f13fa661c82d8317ed6dfd42a3db943e43111094
Balayeva Aytac <abalayeva12@gmail.com>
2025-11-11 18:49:23 +0400

    final deploy prep

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
+import  react  from '@vitejs/plugin-react';
+
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+
 
 
 import tailwindcss from "@tailwindcss/vite";
